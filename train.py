7335501673ac473144da6a87bdde5bb6697729f2
duanzhicheng <zhicheng.duan@yi23.net>
2019-03-30 16:30:28 +0800

    add early stop

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ parser.add_argument("--decay_rate", type=float, default=0.5, help="decaying rate
 
 parser.add_argument("--checkpoint", default=None, help="directory with checkpoint to resume training from")
 parser.add_argument("--output_dir", required=True, help="where to put output files")
-parser.add_argument("--save_freq", type=int, default=5000, help="save model every save_freq steps")
+parser.add_argument("--save_freq", type=int, default=2000, help="save model every save_freq steps")
 parser.add_argument("--tfrecords", required=True, help="training dataset")
 args = parser.parse_args()
 # List of names of the layer trained from scratch
@@ -31,13 +31,14 @@ skip_layers = ['fc8']
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.2
 
-# hash loss function 
+# hash loss function
 def hashing_loss(D, label, alpha, belta, gama, m):
-    label_count = tf.expand_dims(tf.sqrt(tf.reduce_sum(tf.square(label), 1)),1)
-    norm_label = label/tf.tile(label_count,[1,args.num_class])
+    label_count = tf.expand_dims(tf.sqrt(tf.reduce_sum(tf.square(label), 1)), 1)
+    norm_label = label/tf.tile(label_count, [1, args.num_class])
     w_label = tf.matmul(norm_label, norm_label, False, True)
-    semi_label = tf.where(w_label>0.99, w_label-w_label,w_label)
+    semi_label = tf.where(w_label>0.99, w_label-w_label, w_label)
     p2_distance = tf.matmul(D, D, False, True)
     
     scale_distance = belta * p2_distance / m
@@ -45,7 +46,7 @@ def hashing_loss(D, label, alpha, belta, gama, m):
     temp1 = tf.sigmoid(scale_distance)
     loss = tf.where(semi_label<0.01,gama*(temp - w_label * scale_distance), tf.square(w_label-temp1))
     regularizer = tf.reduce_mean(tf.abs(tf.abs(D) - 1))
-    d_loss = tf.reduce_mean(loss) + alpha * regularizer 
+    d_loss = tf.reduce_mean(loss) + alpha * regularizer
     return d_loss,w_label
 
 def main():
@@ -55,7 +56,8 @@ def main():
     # read data from tfrecord files
     img, label = reader.read_and_decode(args.tfrecords, epochs=args.num_epochs, size=args.img_size)
     img_batch, label_batch = tf.train.shuffle_batch([img, label],
-                                                    batch_size=args.batch_size, capacity=2000,
+                                                    batch_size=args.batch_size,
+                                                    capacity=2000,
                                                     min_after_dequeue=1000)
 
     global_step = tf.Variable(0, name="global_step", trainable=False)
@@ -70,18 +72,18 @@ def main():
     # List of trainable variables of the layers to train from scratch
     var_list2 = [v for v in tf.trainable_variables() if v.name.split('/')[0] in skip_layers]
 
-    # learning rate 
+    # learning rate
     learning_rate = tf.train.exponential_decay(args.lr, global_step, args.decay_step,  args.decay_rate, staircase=True)
     opt1 = tf.train.AdamOptimizer(learning_rate * 0.001)
     opt2 = tf.train.AdamOptimizer(learning_rate)
 
-    # apply different grads for two type layers 
+    # apply different grads for two type layers
     grads = tf.gradients(d_loss, var_list1+var_list2)
     grads1 = grads[:len(var_list1)]
-    grads2 = grads[len(var_list1):] 
+    grads2 = grads[len(var_list1):]
     train_op1 = opt1.apply_gradients(zip(grads1,var_list1))
     train_op2 = opt2.apply_gradients(zip(grads2,var_list2), global_step=global_step)
-    train_op = tf.group(train_op1, train_op2)    
+    train_op = tf.group(train_op1, train_op2)
 
     with tf.Session(config=config) as sess:
         saver = tf.train.Saver(tf.global_variables())
@@ -92,7 +94,7 @@ def main():
             saver.restore(sess, checkpoint)
         else:
             # Load the pretrained weights into the non-trainable layer
-            model.load_initial_weights(sess)  
+            model.load_initial_weights(sess)
 
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
@@ -102,11 +104,18 @@ def main():
                 _, loss_t,dt, step1 = sess.run([train_op, d_loss,out, global_step])
                 elapsed_time = time.time() - start_time
                 start_time = time.time()
-
-                if step1 % 10 == 0:
+                if step1 == 1:
+                    min_loss = loss_t
+                    min_step = step1
+                if step1 % 100 == 0:
                     print("iter: %4d, loss: %.8f, time: %.3f" % (step1, loss_t, elapsed_time))
-                if step1 % args.save_freq == 0:
+                if step1 % args.save_freq == 0 and loss_t < min_loss:
+                    min_loss = loss_t
+                    min_step = step1
                     saver.save(sess, args.output_dir + '/model.ckpt', global_step=step1)
+                if step1 - min_step == 3000:
+                    print('Early stopping!', min_step, min_loss)
+                    break
 
         except tf.errors.OutOfRangeError:
             saver.save(sess, args.output_dir + '/model-done.ckpt')
